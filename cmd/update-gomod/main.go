2dfa07fde219f577a84310f5ba7c6ba5b78dc2a4
Masayuki Ishii <masa213f@gmail.com>
2022-07-11 03:02:18 +0000

    update-gomod: use embed
    Signed-off-by: Masayuki Ishii <masa213f@gmail.com>

--- cmd/update-gomod/main.go
+++ cmd/update-gomod/main.go
@@ -1,66 +1,34 @@
 package main
 
 import (
+	_ "embed"
 	"flag"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
 
+	"github.com/goccy/go-yaml"
 	"github.com/masa213f/tools/pkg/util"
 	"golang.org/x/mod/modfile"
 )
 
-type LockRule struct {
-	Name  string
-	Group []PackageGroup
+type Config struct {
+	LockRule []Rule `json:"lock"`
+}
+
+type Rule struct {
+	Name     string         `json:"name"`
+	Packages []PackageGroup `json:"packages"`
 }
 
 type PackageGroup struct {
-	Path []string
-	Tag  string
+	Path []string `json:"path"`
+	Tag  string   `json:"tag"`
 }
 
-var defaultRules = []LockRule{
-	{
-		Name: "k8s",
-		Group: []PackageGroup{
-			{
-				Path: []string{
-					"sigs.k8s.io/controller-runtime",
-				},
-				Tag: "v0.12.3",
-			},
-			{
-				Path: []string{
-					"k8s.io/api",
-					"k8s.io/apiextensions-apiserver",
-					"k8s.io/apimachinery",
-					"k8s.io/apiserver",
-					"k8s.io/client-go",
-					"k8s.io/cli-runtime",
-					"k8s.io/kubectl",
-					"k8s.io/kubelet",
-					"k8s.io/kube-proxy",
-					"k8s.io/kube-scheduler",
-				},
-				Tag: "v0.24.2",
-			},
-		},
-	},
-	{
-		Name: "cni",
-		Group: []PackageGroup{
-			{
-				Path: []string{
-					"github.com/containernetworking/cni",
-					"github.com/containernetworking/plugins",
-				},
-				Tag: "v1.0.1",
-			},
-		},
-	},
-}
+//go:embed config.yaml
+var defaultConfigBytes []byte
 
 func main() {
 	flag.Parse()
@@ -71,14 +39,20 @@ func main() {
 	workDir := flag.Arg(0)
 	fmt.Printf("WORK_DIR: %s\n", workDir)
 
-	err := subMain(workDir, defaultRules)
+	var deufaltConfig Config
+	err := yaml.Unmarshal(defaultConfigBytes, &deufaltConfig)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	err = subMain(workDir, &deufaltConfig)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
 }
 
-func subMain(workDir string, rules []LockRule) error {
+func subMain(workDir string, config *Config) error {
 	data, err := os.ReadFile(filepath.Join(workDir, "go.mod"))
 	if err != nil {
 		return fmt.Errorf("failed to read go.mod; %v", err)
@@ -90,8 +64,8 @@ func subMain(workDir string, rules []LockRule) error {
 
 	packageRuleName := map[string]string{}
 	packageTag := map[string]string{}
-	for _, rule := range defaultRules {
-		for _, group := range rule.Group {
+	for _, rule := range config.LockRule {
+		for _, group := range rule.Packages {
 			for _, pkg := range group.Path {
 				packageRuleName[pkg] = rule.Name
 				packageTag[pkg] = group.Tag
@@ -115,7 +89,7 @@ func subMain(workDir string, rules []LockRule) error {
 		jobs = append(jobs, []string{r.Mod.Path})
 	}
 	for ruleName, packages := range locked {
-		fmt.Printf("Rule %s: %v\n", ruleName, packages)
+		fmt.Printf("LOCK RULE: %s %v\n", ruleName, packages)
 		jobs = append(jobs, packages)
 	}
 
--- go.mod
+++ go.mod
@@ -2,6 +2,15 @@ module github.com/masa213f/tools
 
 go 1.17
 
-require golang.org/x/mod v0.5.1
+require (
+	github.com/goccy/go-yaml v1.9.5
+	golang.org/x/mod v0.5.1
+)
 
-require golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898 // indirect
+require (
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	golang.org/x/sys v0.0.0-20200223170610-d5e6a3e2c0ae // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
